d462f2acf85028ccee449c91b2b195e128de3106
297harsh <120697987+297harsh@users.noreply.github.com>
2024-05-25 08:35:39 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React ,{useState, useEffect} from 'react'
 
 import "./Header.css"
+import { assets } from "../../assets/assets";
+
 
 
 const Header = () => {
+
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    const url = assets.header_img; 
+    setImageUrl(url);
+  }, []); 
+
   return (
-    <div className="header">
+    <div className="header" style={{ background: `url(${imageUrl}) no-repeat` }}>
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
             <p>Choose from a device manu featuring a delectable array of dishes crafted with finest ingrediants and culinary experties.Our mission is satisfy your craving and elevate your dining experience ,one delicious meal at a time.</p>
