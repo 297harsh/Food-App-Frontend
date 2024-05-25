6da501180481e3d017a6ff395cf563d4bd83979c
297harsh <120697987+297harsh@users.noreply.github.com>
2024-05-25 08:21:43 +0530

    Add files via upload

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,18 +1,25 @@
-import React ,{useState} from "react";
+import React ,{useContext} from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
 
-  const [itemCount,setItemCount]= useState(0);
+  const {cartItems,addToCart,removeFromCart} = useContext(StoreContext)
+    
   return (
-    <div className="food-item">
-      <div className="food-item-image-container">
-        <img className="food-item-imgge" src={image} alt="" />
-        {/* {!itemCount
-          ?<img src={assets.add_icon_white} alt="" />
-          :<div>
-        } */}
+
+    <div className="food-item"  >
+      <div className="food-item-img-container">
+        <img className="food-item-image" src={image} alt="" data-aos="flip-up" />
+        {!cartItems[id]
+          ?<img className="add" onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
+          :<div className="food-item-counter">
+            <img onClick={()=>removeFromCart(id)}  src={assets.remove_icon_red} alt="" />
+            <p>{cartItems[id]}</p>
+            <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
+          </div>
+        }
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
@@ -27,3 +34,5 @@ const FoodItem = ({ id, name, price, description, image }) => {
 };
 
 export default FoodItem;
+
+
